e8b37fb746e00e9b29f8b385535b6af22a7062e0
Evgenii Mykhalichenko <emykhailichenko@newage.io>
2022-06-27 22:32:43 +0200

    Update

--- src/Steam.php
+++ src/Steam.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace KrepyshSpec\SteamMarketParser;
 
+use KrepyshSpec\SteamMarketParser\Enum\SteamCurrency;
+
 class Steam
 {
     public function __construct()
     {
-        echo "1";
+        echo SteamCurrency::USD->value;
     }
 }
